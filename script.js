3a7beebb04069037034b942686caa34ac26917f7
Harmit Sonani <89058929+Harmit1708@users.noreply.github.com>
2022-05-19 14:14:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,152 +10,82 @@ const b9 = document.getElementById("b9");
 const print = document.getElementById("print");
 let count = 0;
 function common() {
+  print.innerHTML = flag == 1 ? "Player X Turn" : "Player 0 Turn";
   if (count == 4) {
-    if (b1.value == "X" && b2.value == "X" && b3.value == "X") {
-      print.innerHTML = "Player X Win";
+    if ((b1.value == "X" && b2.value == "X" && b3.value == "X") || (b1.value == "0" && b2.value == "0" && b3.value == "0")) {
+      print.innerHTML = flag == 1 ? "Player 0 Win" : "Player X Win";
       b4.disabled = true;
       b5.disabled = true;
       b6.disabled = true;
       b7.disabled = true;
       b8.disabled = true;
       b9.disabled = true;
-      alert("Player X Win");
-    } else if (b1.value == "X" && b4.value == "X" && b7.value == "X") {
-      print.innerHTML = "Player X Win";
+      flag == 1 ?  alert("Player 0 Win") :  alert("Player X Win");
+    } else if ((b1.value == "X" && b4.value == "X" && b7.value == "X") || (b1.value == "0" && b4.value == "0" && b7.value == "0") ) {
+      print.innerHTML = flag == 1 ? "Player 0 Win" : "Player X Win";
       b2.disabled = true;
       b3.disabled = true;
       b5.disabled = true;
       b6.disabled = true;
       b8.disabled = true;
       b9.disabled = true;
-      alert("Player X Win");
-    } else if (b7.value == "X" && b8.value == "X" && b9.value == "X") {
-      print.innerHTML = "Player X Win";
+      flag == 1 ?  alert("Player 0 Win") :  alert("Player X Win");
+    } else if ((b7.value == "X" && b8.value == "X" && b9.value == "X") || (b7.value == "0" && b8.value == "0" && b9.value == "0")) {
+      print.innerHTML = flag == 1 ? "Player 0 Win" : "Player X Win";
       b1.disabled = true;
       b2.disabled = true;
       b3.disabled = true;
       b4.disabled = true;
       b5.disabled = true;
       b6.disabled = true;
-      alert("Player X Win");
-    } else if (b3.value == "X" && b6.value == "X" && b9.value == "X") {
-      print.innerHTML = "Player X Win";
+      flag == 1 ?  alert("Player 0 Win") :  alert("Player X Win");
+    } else if ((b3.value == "X" && b6.value == "X" && b9.value == "X") || (b3.value == "0" && b6.value == "0" && b9.value == "0")) {
+      print.innerHTML = flag == 1 ? "Player 0 Win" : "Player X Win";
       b1.disabled = true;
       b2.disabled = true;
       b4.disabled = true;
       b5.disabled = true;
       b7.disabled = true;
       b8.disabled = true;
-      alert("Player X Win");
-    } else if (b1.value == "X" && b5.value == "X" && b9.value == "X") {
-      print.innerHTML = "Player X Win";
+      flag == 1 ?  alert("Player 0 Win") :  alert("Player X Win");
+    } else if ((b1.value == "X" && b5.value == "X" && b9.value == "X") || (b1.value == "0" && b5.value == "0" && b9.value == "0")) {
+      print.innerHTML = flag == 1 ? "Player 0 Win" : "Player X Win";
       b2.disabled = true;
       b3.disabled = true;
       b4.disabled = true;
       b6.disabled = true;
       b7.disabled = true;
       b8.disabled = true;
-      alert("Player X Win");
-    } else if (b3.value == "X" && b5.value == "X" && b7.value == "X") {
-      print.innerHTML = "Player X Win";
+      flag == 1 ?  alert("Player 0 Win") :  alert("Player X Win");
+    } else if ((b3.value == "X" && b5.value == "X" && b7.value == "X") || (b3.value == "0" && b5.value == "0" && b7.value == "0")) {
+      print.innerHTML = flag == 1 ? "Player 0 Win" : "Player X Win";
       b1.disabled = true;
       b2.disabled = true;
       b4.disabled = true;
       b6.disabled = true;
       b8.disabled = true;
       b9.disabled = true;
-      alert("Player X Win");
-    } else if (b2.value == "X" && b5.value == "X" && b8.value == "X") {
-      print.innerHTML = "Player X Win";
-      b1.disabled = true;
-      b3.disabled = true;
-      b4.disabled = true;
-      b6.disabled = true;
-      b7.disabled = true;
-      b9.disabled = true;
-      alert("Player X Win");
-    } else if (b4.value == "X" && b5.value == "X" && b6.value == "X") {
-      print.innerHTML = "Player X Win";
-      b1.disabled = true;
-      b2.disabled = true;
-      b3.disabled = true;
-      b7.disabled = true;
-      b8.disabled = true;
-      b9.disabled = true;
-      alert("Player X Win");
-    } else if (b1.value == "0" && b2.value == "0" && b3.value == "0") {
-      print.innerHTML = "Player 0 Win";
-      b4.disabled = true;
-      b5.disabled = true;
-      b6.disabled = true;
-      b7.disabled = true;
-      b8.disabled = true;
-      b9.disabled = true;
-      alert("Player 0 Win");
-    } else if (b1.value == "0" && b4.value == "0" && b7.value == "0") {
-      print.innerHTML = "Player 0 Win";
-      b2.disabled = true;
-      b3.disabled = true;
-      b5.disabled = true;
-      b6.disabled = true;
-      b8.disabled = true;
-      b9.disabled = true;
-      alert("Player 0 Win");
-    } else if (b7.value == "0" && b8.value == "0" && b9.value == "0") {
-      print.innerHTML = "Player 0 Win";
-      b1.disabled = true;
-      b2.disabled = true;
-      b3.disabled = true;
-      b4.disabled = true;
-      b5.disabled = true;
-      b6.disabled = true;
-      alert("Player 0 Win");
-    } else if (b3.value == "0" && b6.value == "0" && b9.value == "0") {
-      print.innerHTML = "Player 0 Win";
-      b1.disabled = true;
-      b2.disabled = true;
-      b4.disabled = true;
-      b5.disabled = true;
-      b7.disabled = true;
-      b8.disabled = true;
-      alert("Player 0 Win");
-    } else if (b1.value == "0" && b5.value == "0" && b9.value == "0") {
-      print.innerHTML = "Player 0 Win";
-      b2.disabled = true;
-      b3.disabled = true;
-      b4.disabled = true;
-      b6.disabled = true;
-      b7.disabled = true;
-      b8.disabled = true;
-      alert("Player 0 Win");
-    } else if (b3.value == "0" && b5.value == "0" && b7.value == "0") {
-      print.innerHTML = "Player 0 Win";
-      b1.disabled = true;
-      b2.disabled = true;
-      b4.disabled = true;
-      b6.disabled = true;
-      b8.disabled = true;
-      b9.disabled = true;
-      alert("Player 0 Win");
-    } else if (b2.value == "0" && b5.value == "0" && b8.value == "0") {
-      print.innerHTML = "Player 0 Win";
+      flag == 1 ?  alert("Player 0 Win") :  alert("Player X Win");
+    } else if ((b2.value == "X" && b5.value == "X" && b8.value == "X") || (b2.value == "0" && b5.value == "0" && b8.value == "0")) {
+      print.innerHTML = flag == 1 ? "Player 0 Win" : "Player X Win";
       b1.disabled = true;
       b3.disabled = true;
       b4.disabled = true;
       b6.disabled = true;
       b7.disabled = true;
       b9.disabled = true;
-      alert("Player 0 Win");
-    } else if (b4.value == "0" && b5.value == "0" && b6.value == "0") {
-      print.innerHTML = "Player 0 Win";
+      flag == 1 ?  alert("Player 0 Win") :  alert("Player X Win");
+    } else if ((b4.value == "X" && b5.value == "X" && b6.value == "X") || (b4.value == "0" && b5.value == "0" && b6.value == "0")) {
+      print.innerHTML = flag == 1 ? "Player 0 Win" : "Player X Win";
       b1.disabled = true;
       b2.disabled = true;
       b3.disabled = true;
       b7.disabled = true;
       b8.disabled = true;
       b9.disabled = true;
-      alert("Player 0 Win");
-    } else if (
+      flag == 1 ?  alert("Player 0 Win") :  alert("Player X Win");
+    } 
+    else if (
       (b1.value == "X" || b1.value == "0") &&
       (b2.value == "X" || b2.value == "0") &&
       (b3.value == "X" || b3.value == "0") &&
@@ -170,11 +100,6 @@ function common() {
       alert("Match Tie");
     }
   }else {
-    if (flag == 1) {
-      print.innerHTML = "Player X Turn";
-    } else {
-      print.innerHTML = "Player 0 Turn";
-    }
     count++;
   } 
 }
@@ -198,6 +123,7 @@ function fun_3() {
   newOne.value = flag == 1 ? "X" : "0";
   newOne.disabled = true;
   flag = flag == 1 ? 0 : 1;
+  
 }
 
 function fun_4() {
@@ -205,6 +131,7 @@ function fun_4() {
   newOne.value = flag == 1 ? "X" : "0";
   newOne.disabled = true;
   flag = flag == 1 ? 0 : 1;
+
 }
 
 function fun_5() {
